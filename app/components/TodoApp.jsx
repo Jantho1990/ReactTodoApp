8cbf63483f8d976f7dccef4946c9e509ddf11eaa
Joshua Anthony <jantho1990@gmail.com>
2018-01-03 21:47:53 -0600

    Added basic app stuff

---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,13 @@
+const React = require('react');
+
+let TodoApp = React.createClass({
+    render: function(){
+        return (
+            <div>
+                TodoApp.jsx
+            </div>
+        )
+    }
+})
+
+module.exports = TodoApp
